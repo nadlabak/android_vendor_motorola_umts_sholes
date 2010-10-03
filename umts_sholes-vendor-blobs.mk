5e80d5e3b2762ade7f86bc3b5878ccd7d45f70f2
nadlabak <pavel@doshaska.net>
2010-10-03 16:26:21 -0700

    correct the spaces in the file name (Toggle 2G Notification Plugin)

--- umts_sholes-vendor-blobs.mk
+++ umts_sholes-vendor-blobs.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/umts_sholes/proprietary/bin/init_kill_old_adbd.sh:/system/bin/init_kill_old_adbd.sh \
     vendor/motorola/umts_sholes/proprietary/bin/init_prep_keypad.sh:/system/bin/init_prep_keypad.sh \
     vendor/motorola/umts_sholes/proprietary/bin/Toggle2G.apk:/system/bin/Toggle2G.apk \
-    vendor/motorola/umts_sholes/proprietary/bin/Toggle 2G Notification Plugin.apk:/system/bin/Toggle 2G Notification Plugin.apk \
+    vendor/motorola/umts_sholes/proprietary/bin/Toggle_2G_Notification_Plugin.apk:/system/bin/Toggle_2G_Notification_Plugin.apk \
     vendor/motorola/umts_sholes/proprietary/etc/rootfs/init.mapphone_umts.rc:/system/etc/rootfs/init.mapphone_umts.rc \
     vendor/motorola/umts_sholes/proprietary/etc/wifi/wpa_supplicant.conf:/system/etc/wifi/wpa_supplicant.conf \
     vendor/motorola/umts_sholes/proprietary/etc/wifi/fw_wlan1271.bin:/system/etc/wifi/fw_wlan1271.bin \
